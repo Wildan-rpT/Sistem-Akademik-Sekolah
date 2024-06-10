54a423979a28ec39cbf1a4363b5b765d396f5be6
Anggiat Parasian Damanik <anggiatdmnk03@gmail.com>
2024-06-10 22:47:18 +0700

    initial commit

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller
+class Siswa extends CI_Controller
 {
 	public function index()
 	{
 		$this->load->view('templates_menu/header');
 		$this->load->view('templates_menu/sidebar');
-		$this->load->view('menu/home');
+		$this->load->view('menu/siswa');
 		$this->load->view('templates_menu/footer');
 	}
 }
