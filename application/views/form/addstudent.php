7175fc13ee39a73aebdca7a5668821d80cfcf6e7
Anggiat Parasian Damanik <anggiatdmnk03@gmail.com>
2024-06-13 10:24:13 +0700

    form tambah siswa

--- application/views/form/addstudent.php
+++ application/views/form/addstudent.php
@@ -12,44 +12,50 @@
             <div class="row mb-3">
                 <label for="nisn" class="col-sm-2 col-form-label">NISN</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="nisn" name="nisn">
+                    <input type="text" class="form-control" id="nisn" name="nisn" required>
                 </div>
             </div>
 
             <div class="row mb-3">
                 <label for="nama" class="col-sm-2 col-form-label">Nama</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="nama" name="nama">
+                    <input type="text" class="form-control" id="nama" name="nama" required>
                 </div>
             </div>
 
             <div class="row mb-3">
                 <label for="jenis_kelamin" class="col-sm-2 col-form-label">Jenis Kelamin</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="jenis_kelamin" name="jenis_kelamin">
+                    <input type="text" class="form-control" id="jenis_kelamin" name="jenis_kelamin" required>
                 </div>
             </div>
 
             <div class="row mb-3">
                 <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="alamat" name="alamat">
+                    <input type="text" class="form-control" id="alamat" name="alamat" required>
                 </div>
             </div>
 
             <div class="row mb-3">
                 <label for="no_hp" class="col-sm-2 col-form-label">Nomor Telepon</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="no_hp" name="no_hp">
+                    <input type="text" class="form-control" id="no_hp" name="no_hp" required>
                 </div>
             </div>
 
             <div class="row mb-3">
                 <label for="foto" class="col-sm-2 col-form-label">Foto</label>
                 <div class="col-sm-10">
-                    <input class="form-control" type="file" id="foto" name="foto">
+                    <input class="form-control" type="file" id="foto" name="foto" required>
+                </div>
+            </div>
+            <div class="form-group row justify-content-end">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Tambahkan</button>
                 </div>
             </div>
         </div>
+
     </div>
 </div>
